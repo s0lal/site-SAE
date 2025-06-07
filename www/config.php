dbc9eb6ab279a92ae6c4af8044f90ed23eb0d0dc
s0lal <solal.reboullet@gmail.com>
2025-06-07 14:47:26 +0200

    Update database configuration to use standard values

--- www/config.php
+++ www/config.php
@@ -1,16 +1,16 @@
 <?php
 
 /** The name of the database for our website */
-define( 'DB_NAME', 'cdce5547_2025_nova' );
+define( 'DB_NAME', 'nova_database' );
 
 /** Database username */
-define( 'DB_USER', 'cdce5547_admin_sql_nova' );
+define( 'DB_USER', 'user' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'MMI4ever@BDD' );
+define( 'DB_PASSWORD', 'userpassword' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
