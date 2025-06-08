09e4ebbfd1758435c78ed7afd6197d644be5256c
s0lal <solal.reboullet@gmail.com>
2025-06-08 10:43:53 +0200

    CONFIG

--- www/config.php
+++ www/config.php
@@ -1,16 +1,16 @@
 <?php
 
 /** The name of the database for our website */
-define( 'DB_NAME', 'nova_database' );
+define( 'DB_NAME', 'cdce5547_2025_nova' );
 
 /** Database username */
-define( 'DB_USER', 'user' );
+define( 'DB_USER', 'cdce5547_admin_sql_nova' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'userpassword' );
+define( 'DB_PASSWORD', 'MMI4ever@BDD' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
